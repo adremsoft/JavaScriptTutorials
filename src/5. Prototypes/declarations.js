86a89c02ba75d66fee560e8038e6c0a5870fa60a
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2017-07-19 12:34:09 +0200

    Add tutorial for prototypes

---
+++ src/5. Prototypes/declarations.js
@@ -0,0 +1,157 @@
+/**
+ * Author: Boguslaw Gorczyca
+ * Created: 2017-07-18
+ *
+ * No part of this file may be duplicated, revised, translated,
+ * localized or modified in any manner or compiled, linked or
+ * uploaded or downloaded to or from any computer system without
+ * the prior written consent of AdRem Software sp z o.o.
+ *
+ * 2017 Copyright AdRem Software, all rights reserved
+ */
+
+'use strict';
+
+const debug = require('debug');
+
+// Classes
+
+//  Basic class pattern
+
+function BasicClass(id) {   // Class constructor
+    const
+        self = this, // Private field
+        debugBasicClass = debug('Basic class');
+
+    // Public field
+    this.idMessagePrefix = 'Id:';
+
+    //Private method
+    function getIdMessage() {
+        return `${self.idMessagePrefix} ${id}`;
+    }
+
+    // Public method
+    this.showId = function() {
+        debugBasicClass(getIdMessage());
+    };
+
+    debugBasicClass('Constructor executed');
+}
+
+// Factory method pattern
+
+function FactoryMethod(instanceId) {
+    const debugFactoryMethod = debug('Factory method');  // Private field
+
+    //Private method
+    function getIdMessage(messagePrefix) {
+        return `${messagePrefix} ${instanceId}`;
+    }
+
+    debugFactoryMethod('Executed');
+    return {
+        // Public field
+        idMessagePrefix: 'Id:',
+
+        //Public method
+        showId: function() {
+            debugFactoryMethod(getIdMessage(this.idMessagePrefix));
+        }
+    };
+}
+
+// Class with prototype pattern
+
+function getIdMessage(id) {
+    return `Id: ${id}`;
+}
+
+function ClassWithPrototype(id) {
+    this.debugClass = debug('Class with prototype');
+
+    this.id = id;   //Public field
+
+    this.debugClass('Constructor executed');
+}
+
+ClassWithPrototype.prototype.showId = function() {      // Public method
+    this.debugClass(getIdMessage(this.id));
+};
+
+// Extension of ClassWithPrototype
+
+function ClassExtension(id) {
+    ClassWithPrototype.call(this, id);
+    this.debugClass = debug('ClassExtension');
+    this.debugClass('Constructor executed');
+}
+
+ClassExtension.prototype = Object.create(ClassWithPrototype.prototype);
+
+ClassExtension.prototype.checkExtension = function() {
+    return 'Extension method';
+};
+
+// ES6 Class
+
+class ES6Class {
+
+    constructor(id) {
+        this.debugES6Class = debug('ES6 Class');
+
+        this.id = id;
+
+        this.debugES6Class('Constructor executed');
+    }
+
+    showId() {
+        this.debugES6Class(getIdMessage(this.id));
+    }
+
+}
+
+class ES6ClassExtension extends ES6Class {
+
+    constructor(id) {
+        super(id);
+        this.debugES6Class = debug('ES6 Class Extension');
+        this.debugES6Class('Constructor executed');
+    }
+
+    checkExtension() {
+        return 'ES6 extension method';
+    }
+
+}
+
+/*
+  Singleton pattern
+ */
+
+const singleton = (function () {
+    const debugSingleton = debug('Debug singleton');
+
+    function privateMethod() {
+        return 'Hello from singleton';
+    }
+
+    // Definition of singleton
+    return {
+        publicMethod: function () {
+            debugSingleton(privateMethod());
+        },
+        publicProperty: 'test'
+    };
+
+})();
+
+module.exports = {
+    BasicClass,
+    FactoryMethod,
+    ClassWithPrototype,
+    ClassExtension,
+    ES6Class,
+    ES6ClassExtension,
+    singleton
+};
---
+++ src/5. Prototypes/usage.js
@@ -0,0 +1,95 @@
+/**
+ * Author: Boguslaw Gorczyca
+ * Created: 2017-07-18
+ *
+ * No part of this file may be duplicated, revised, translated,
+ * localized or modified in any manner or compiled, linked or
+ * uploaded or downloaded to or from any computer system without
+ * the prior written consent of AdRem Software sp z o.o.
+ *
+ * 2017 Copyright AdRem Software, all rights reserved
+ */
+
+'use strict';
+
+const
+    {BasicClass, FactoryMethod, ClassWithPrototype, ClassExtension,
+     ES6Class, ES6ClassExtension, singleton} = require('./declarations'),
+    debug = require('debug'),
+    globalDebug = debug('Global debug:');
+
+function useBasicClass() {
+    globalDebug('Create BasicClass instance');
+    const basicClassInstance = new BasicClass(1);
+    globalDebug('Instance of BasicClass created');
+
+    basicClassInstance.showId();
+    basicClassInstance.idMessagePrefix = 'New id prefix: ';
+    basicClassInstance.showId();
+}
+
+function useFactoryMethod() {
+    globalDebug('Create instance by factory method');
+    const instance = new FactoryMethod(2);
+    globalDebug('Instance created');
+
+    instance.showId();
+    instance.idMessagePrefix = 'New id prefix: ';
+    instance.showId();
+}
+
+function useClassWithPrototype() {
+    globalDebug('Create ClassWithPrototype instance');
+    const instanceWithPrototype = new ClassWithPrototype(3);
+    globalDebug('Instance of ClassWithPrototype created');
+
+    instanceWithPrototype.showId();
+    globalDebug(`Instance of ClassWithPrototype: ${(instanceWithPrototype instanceof ClassWithPrototype)}`);
+    globalDebug(`Instance of ClassExtension: ${(instanceWithPrototype instanceof ClassExtension)}`);
+}
+
+function useClassExtension() {
+    globalDebug('Create ClassExtension instance');
+    const instanceOfClassExtension = new ClassExtension(4);
+    globalDebug('Instance of ClassExtension created');
+
+    instanceOfClassExtension.showId();
+    globalDebug(instanceOfClassExtension.checkExtension());
+    globalDebug(`Instance of ClassWithPrototype: ${(instanceOfClassExtension instanceof ClassWithPrototype)}`);
+    globalDebug(`Instance of ClassExtension: ${(instanceOfClassExtension instanceof ClassExtension)}`);
+}
+
+function useES6Class() {
+    globalDebug('Create ES6Class instance');
+    const instanceOfES6Class = new ES6Class(5);
+    globalDebug('Instance of ES6Class created');
+
+    instanceOfES6Class.showId();
+    globalDebug(`Instance of ES6Class: ${(instanceOfES6Class instanceof ES6Class)}`);
+    globalDebug(`Instance of ES6ClassExtension: ${(instanceOfES6Class instanceof ES6ClassExtension)}`);
+}
+
+function useES6ClassExtension() {
+    globalDebug('Create ES6ClassExtension instance');
+    const instanceOfES6ClassExtension = new ES6ClassExtension(6);
+    globalDebug('Instance of ES6ClassExtension created');
+
+    instanceOfES6ClassExtension.showId();
+    globalDebug(instanceOfES6ClassExtension.checkExtension());
+    globalDebug(`Instance of ES6Class: ${(instanceOfES6ClassExtension instanceof ES6Class)}`);
+    globalDebug(`Instance of ES6ClassExtension: ${(instanceOfES6ClassExtension instanceof ES6ClassExtension)}`);
+}
+
+function useSingleton() {
+    globalDebug('Using singleton');
+    singleton.publicMethod();
+    globalDebug(singleton.publicProperty);
+}
+
+useBasicClass();
+useFactoryMethod();
+useClassWithPrototype();
+useClassExtension();
+useES6Class();
+useES6ClassExtension();
+useSingleton();
